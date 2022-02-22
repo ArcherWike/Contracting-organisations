245d625ee107da913ae752a8d960706c88ed0374
Wiktoria <wiktoria.szczepanska.x@gmail.com>
2022-02-22 21:02:53 +0100

    New version 1.7.

---
+++ Contracting-organisations-v1.7/decorations.py
@@ -0,0 +1,76 @@
+import space
+#############- Decoration and display functions -#########################
+
+def ui():
+    print("="*50)
+
+def operations():
+    ui()
+    menu = [
+        'Edytuj notatki',
+        'Notatki',
+        'Edytuj adresy',
+        'Adresy',
+        'Wyjdz'
+    ]
+    for x, an in enumerate(menu):
+        print("\t",x+1 , an)
+    ui()
+
+def menu_rooms(do):
+    ui()
+    for x, an in enumerate(space.room[0:len(space.room)]):
+        if (do == False):
+            if(len(space.room) == 1):
+                print("Brak zapisanych adresow!")
+                return
+            if(x == 0):
+                pass
+            else:
+                print("\t", x, an[0])
+        if do:
+            if(x == 0):
+                print("\t", x, an)
+            else:
+                print("\t", x, an[0])
+    ui()
+
+
+def rooms_option():
+    ui()
+    space_menu = [
+        'zobacz miejsca',
+        'dodaj adres',
+        'usun adres'
+    ]
+
+    for x, an in enumerate(space_menu):
+        print("\t",x+1, an)
+    ui()
+
+def teq(do):
+    ui()
+    quterm = ['Dopisz termin wykonania',
+              'Nie dodawaj',
+              'O co chodzi?']
+
+    for x, an in enumerate(quterm):
+        if(do == False):
+            if(x == 2):
+                pass
+            else:
+                print("\t", x + 1, an)
+        else:
+            print("\t", x + 1, an)
+    ui()
+
+
+def see_menu():
+    ui()
+    print("|\t\t\tMENU\t\t\t|")
+    ui()
+
+
+def display_notes(index):
+    for y in range(1, len(space.room[index])):
+        print('\t', y, (space.room[index][y]))
